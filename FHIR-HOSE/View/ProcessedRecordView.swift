bc7f015d2cdbc9884d5ce784d1568b3738a257c7
ehurwitz <erichur2@gmail.com>
2025-02-24 18:14:28 -0500

    Added ProcessedRecordView

---
+++ FHIR-HOSE/View/ProcessedRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  ProcessedRecordView.swift
+//  FHIR-HOSE
+//
+//  Created by Eric Hurwitz on 2/24/25.
+//
+
+import SwiftUI
+
+struct ProcessedRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProcessedRecordView()
+}
